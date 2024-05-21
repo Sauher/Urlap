2cb669b6ebebf6c1870b2039a0933a2ad0415a78
Sauher <schaueroliver@turr.hu>
2024-05-21 12:29:21 +0200

    Javascript elkezdése html befejezése

---
+++ foglalas.js
@@ -0,0 +1,5 @@
+function Kalkulal(){
+    let erk = new Date(document.getElementById("erkezes").value)
+    let 
+    alert("Kedves vendégünk!\n\nTájékoztatjuk sikeresfolgalásáról.\n\nÉrkezés: "+erk.getFullYear()+"-"+erk.getMonth()+"-"+erk.getDay())
+}
