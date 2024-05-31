a6c2d7dfad384c433cc25d0d84b85cf8eb9dce8e
Sauher <schaueroliver@turr.hu>
2024-05-31 09:34:31 +0200

    Oldal befejezve

--- foglalas.js
+++ foglalas.js
@@ -24,6 +24,16 @@ function Valtozas(){
         document.getElementById("ev3").disabled = false
         document.getElementById("ev4").disabled = false
     }
+    if(document.getElementById("teljes").checked){
+        document.getElementById("kmedence").disabled = true
+        document.getElementById("bmedence").disabled = true 
+        document.getElementById("szauna").disabled = true
+    }
+    else{
+        document.getElementById("kmedence").disabled = false
+        document.getElementById("bmedence").disabled = false 
+        document.getElementById("szauna").disabled = false
+    }
 }
 function Kalkulal(){
     let vendegszam = document.getElementById("vendegszan").value
@@ -35,69 +45,69 @@ function Kalkulal(){
     let tavev = tav.getFullYear().toString()
     let tavhonap = tav.getMonth()+1
     let tavnap = tav.getDate().toString()
-    let idotartam = Number((((+tav)-(+erk)) / 3600000)/24)
+    let idotartam = (((+tav)-(+erk)) / 3600000)/24
     let szobatipus
-    let osszeg
-    let ellatas
-    let szolgaltatas
+    let osszeg = 0
+    let ellatas 
+    let szolgaltatas = " "
 
 
     if(document.getElementById("egyagy").checked == true)
     {
         szobatipus = document.getElementById("egyagy").value
-        osszeg+= 9000*Number(idotartam)
+        osszeg+= 9000*idotartam
     }
     else if(document.getElementById("kettoagy").checked == true)
     {
         szobatipus = document.getElementById("kettoagy").value
-        osszeg+= 15000*Number(idotartam)
+        osszeg+= 15000*idotartam
     }
     else if(document.getElementById("kettoagypluszegy").checked == true)
     {
         szobatipus = document.getElementById("kettoagypluszegy").value
-        osszeg+= 18000*Number(idotartam)
+        osszeg+= 18000*idotartam
     }
     else if(document.getElementById("kettoagypluszketto").checked == true)
     {
         szobatipus = document.getElementById("kettoagypluszketto").value
-        osszeg+= 21000*Number(idotartam)
+        osszeg+= 21000*idotartam
     }
 
     if(document.getElementById("reggeli").checked == true)
     {
         ellatas = document.getElementById("reggeli").value
-        osszeg+= 900*Number(idotartam)*Number(vendegszam)
+        osszeg += 900*idotartam*vendegszam
     }
     else if(document.getElementById("felpanzio").checked == true)
     {
         ellatas = document.getElementById("felpanzio").value
-        osszeg+= 2900*Number(idotartam)*Number(vendegszam)
+        osszeg += 2900*idotartam*vendegszam
     }
     else if(document.getElementById("panzio").checked == true)
     {
         ellatas = document.getElementById("panzio").value
-        osszeg+= 4900*Number(idotartam)*Number(vendegszam)
+        osszeg += 4900*idotartam*vendegszam
     }
-    if(document.getElementById("reggeli").checked == true)
+    if(document.getElementById("bmedence").checked == true)
     {
-        ellatas = document.getElementById("reggeli").value
-        osszeg+= 900*Number(idotartam)*Number(vendegszam)
+        szolgaltatas = document.getElementById("bmedence").value
+        osszeg += 900*idotartam*vendegszam
     }
-    else if(document.getElementById("felpanzio").checked == true)
+    if(document.getElementById("kmedence").checked == true)
     {
-        ellatas = document.getElementById("felpanzio").value
-        osszeg+= 2900*Number(idotartam)*Number(vendegszam)
+        szolgaltatas += " "+document.getElementById("kmedence").value
+        osszeg += 2900*idotartam*vendegszam
     }
-    else if(document.getElementById("panzio").checked == true)
+    if(document.getElementById("szauna").checked == true)
     {
-        ellatas = document.getElementById("panzio").value
-        osszeg+= 4900*Number(idotartam)*Number(vendegszam)
+        szolgaltatas += " "+document.getElementById("szauna").value
+        osszeg += 4900*idotartam*vendegszam
     }  
-    else if(document.getElementById("panzio").checked == true)
+    if(document.getElementById("teljes").checked == true)
     {
-        ellatas = document.getElementById("panzio").value
-        osszeg+= 4900*Number(idotartam)*Number(vendegszam)
+        szolgaltatas = document.getElementById("teljes").value
+        osszeg += 4900*idotartam*vendegszam
     }
 
-    alert("Kedves vendégünk!\n\nTájékoztatjuk sikeresfolgalásáról.\n\nÉrkezés: "+erkev+"-"+erkhonap+"-"+erknap+"\n\nTávozás: "+tavev+"-"+tavhonap+"-"+tavnap+"\n\nSzoba típusa: "+szobatipus+"\n\nVendégek száma: "+vendegszam+"\n\nEllátás: "+ellatas+"\n\nIgénylet szolgáltatások: "+szolgaltatas+"\n\nA teljes össeg: "+osszeg)
+    alert("Kedves vendégünk!\n\nTájékoztatjuk sikeres folgalásáról.\n\nÉrkezés: "+erkev+"-"+erkhonap+"-"+erknap+"\n\nTávozás: "+tavev+"-"+tavhonap+"-"+tavnap+"\n\nSzoba típusa: "+szobatipus+"\n\nVendégek száma: "+vendegszam+"\n\nEllátás: "+ellatas+"\n\nIgénylet szolgáltatások: "+szolgaltatas+"\n\nA teljes össeg: "+osszeg)
 }
